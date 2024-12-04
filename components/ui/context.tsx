8b4a7bc35d4286af02c5b276003bbb2f3fa5fb63
Nlucky320 <luckyes144@gmail.com>
2024-12-04 21:59:45 +0600

    ui provider

---
+++ components/ui/context.tsx
@@ -0,0 +1,26 @@
+import { createContext, FC, useContext, useState } from "react"
+
+
+
+const UIContext = createContext<{[key: string]: any}>({
+  uiState: "defaultState"
+})
+
+export const UIProvider: FC = ({children}) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false)
+  const uiState = {
+    isSidebarOpen,
+    setSidebarOpen
+  }
+  return (
+ 
+    <UIContext.Provider value={uiState}>
+      {children}
+    </UIContext.Provider>
+  )
+}
+
+export const useUI = () => {
+  const context = useContext(UIContext)
+  return context
+}
