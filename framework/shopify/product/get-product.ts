0b482ae244d1e86af396a27ed7e61639a63c28a3
Nlucky320 <luckyes144@gmail.com>
2024-12-08 21:43:48 +0600

    specify variable types

--- framework/shopify/product/get-product.ts
+++ framework/shopify/product/get-product.ts
@@ -1,13 +1,22 @@
-import { ApiConfig } from "@common/types/api";
+import { ApiConfig, Variables } from "@common/types/api";
 import { getProductQuery } from "@framework/utils";
+import { Product as ShopifyProduct } from "@framework/schema"
+
+type FetchType = {
+  productByHandle: ShopifyProduct
+}
 
 
-const getProduct=async(options:{config:ApiConfig, variables:any}):Promise<any>=>{
+const getProduct=async(options:{
+    config:ApiConfig, 
+    variables:Variables
+}):Promise<any>=>{
     const {config, variables}=options
     const {data}=await config.fetch<any>({
         query:getProductQuery,url:config.apiUrl, variables
     })
     // console.log(JSON.stringify(data,null,2))
+    console.log(JSON.stringify(data.productByHandle,null,2))
     return{
           product: {
       name: "MY super product",
