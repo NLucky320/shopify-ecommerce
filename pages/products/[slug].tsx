bd4b42eb77346d924d5f4851ba86995528a5ce04
Nlucky320 <luckyes144@gmail.com>
2024-12-10 20:43:05 +0600

    product view

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,4 +1,5 @@
 import { Layout } from "@components/common";
+import { ProductView } from "@components/product";
 import { Container } from "@components/ui";
 import { getConfig } from "@framework/api/config";
 import { getAllProductsPaths, getProduct } from "@framework/product";
@@ -32,46 +33,9 @@ export default function ProductSlug({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   console.log(JSON.stringify(product, null, 2))
   return (
-     <Container>
-      <p>id: {product?.id}</p>
-      <p>name: {product?.name}</p>
-      <p>price value: {product?.price.value}</p>
-      <p>price currency: {product?.price.currencyCode}</p>
-      <p>description: {product?.description}</p>
-      <h1 className="mb-4">OPTIONS</h1>
-      <div>
-        { product?.options.map(option =>
-          <div>
-            <p>Name: {option.displayName}</p>
-            { option.values.map(value =>
-              <div>
-                <p>Label: {value.label}</p>
-                <p>Hex Color: {value.hexColor}</p>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-      <h1 className="mb-4">VARIANTS</h1>
-      <div>
-        { product?.variants.map(variant =>
-          <div>
-            <p>Variant Name: {variant.name}</p>
-            { variant?.options?.map(vo =>
-              <div>
-                <p>Name: {vo.displayName}</p>
-                { vo.values.map(value =>
-                  <div>
-                    <p>Label: {value.label}</p>
-                    <p>Hexcolor: {value.hexColor}</p>
-                  </div>
-                )}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    </Container>
+     <>
+        { product && <ProductView product={product} />}
+    </>
   );
 }
 
