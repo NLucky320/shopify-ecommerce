560307d5fa281828ae5e26b7c59a8ea0caa39067
Nlucky320 <luckyes144@gmail.com>
2025-01-05 18:31:48 +0600

    usehook handler

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,2 +1,12 @@
 import { useAddItem } from "@common/cart";
 export default useAddItem;
+
+export const handler={
+    useHook:()=>{
+        return (input: any) => {
+    return {
+      output: JSON.stringify(input) + "_MODIFIED"
+    }
+  }
+    }
+}
