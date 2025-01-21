5fb0383dfd09cb831e8c9300a99e6073e4bb33bc
Nlucky320 <luckyes144@gmail.com>
2025-01-21 22:47:56 +0600

    checkout create prep

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -1,40 +1,29 @@
+import useCart from "@common/cart/use-cart";
+import { createCheckout } from "@framework/utils";
 
-
-import useCart from "@common/cart/use-cart"
-import { createCheckout } from "@framework/utils"
-
-export default useCart
+export default useCart;
 
 export const handler = {
   fetchOptions: {
-      query: "query { hello }"
+    query: "query { hello }",
   },
-  async fetcher({
-    fetch, 
-    options,
-  input:{checkoutId}
-  }: any) {
-    // Get checkout
-        let checkout
-
-    // If there is no checkout then create checkout
-      if (checkoutId) {
+  async fetcher({ fetch, options, input: { checkoutId } }: any) {
+    let checkout;
+    if (checkoutId) {
       const { data } = await fetch({
-        ...options
-      })
-
-      checkout = data.node
+        ...options,
+      });
+      checkout = data.node;
     } else {
-      checkout = await createCheckout()
+      checkout = await createCheckout();
     }
 
-    return checkout
-
+    return checkout;
   },
-  useHook: ({useData}: any) => {
-    const data = useData()
+  useHook: ({ useData }: any) => {
+    const data = useData();
     return {
-      data
-    }
-  }
-}
+      data,
+    };
+  },
+};
