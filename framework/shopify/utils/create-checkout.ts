93c2f68e706ed22969f5bf4d5d9b7da5e9df48af
Nlucky320 <luckyes144@gmail.com>
2025-01-22 22:21:25 +0600

    extraxt data from checkout create

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,17 +1,20 @@
 import { ApiFetcher } from "@common/types/api"
 import { checkoutCreateMutation } from "./mutation"
+import { Checkout, CheckoutCreatePayload, Maybe } from "@framework/schema"
 
 const createCheckout = async (
-  fetch: ApiFetcher
-): Promise<any> => {
-
+  fetch: ApiFetcher<{checkoutCreate: CheckoutCreatePayload}>
+): Promise<Maybe<Checkout | undefined>> => {
   const { data } = await fetch({
     query: checkoutCreateMutation
   })
 
+  const { checkout } = data.checkoutCreate
+  const checkoutId = checkout?.id
 
-  return data
+
+
+  return checkout
 }
-
 
 export default createCheckout
