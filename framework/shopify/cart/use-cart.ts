d99707d3b4f35586eb0a2e377e26c624c4037926
Nlucky320 <luckyes144@gmail.com>
2025-01-24 22:12:44 +0600

    get checkout query

--- framework/shopify/cart/use-cart.ts
+++ framework/shopify/cart/use-cart.ts
@@ -1,17 +1,20 @@
 import useCart from "@common/cart/use-cart";
-import { createCheckout } from "@framework/utils";
+import { createCheckout,  getCheckoutQuery } from "@framework/utils";
 
 export default useCart;
 
 export const handler = {
   fetchOptions: {
-    query: "query { hello }",
+      query: getCheckoutQuery
   },
   async fetcher({ fetch, options, input: { checkoutId } }: any) {
     let checkout;
     if (checkoutId) {
       const { data } = await fetch({
-        ...options,
+           ...options,
+        variables: {
+          checkoutId
+        }
       });
       checkout = data.node;
     } else {
