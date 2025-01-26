0560fa1cf1e51608c61f4c2847b09fab7b944b34
Nlucky320 <luckyes144@gmail.com>
2025-01-26 21:34:59 +0600

    useSWR improvements and useMemo

--- components/common/Usernav/Usernav.tsx
+++ components/common/Usernav/Usernav.tsx
@@ -3,9 +3,11 @@ import s from "./UserNav.module.css"
 import Link from "next/link"
 import { Bag as Cart, Heart } from "@components/icons"
 import { useUI } from "@components/ui/context"
+import useCart from "@common/cart/use-cart"
 
 const UserNav: FC = () => {
   const ui=useUI()
+    const { data } = useCart()
 
   return (
     <nav>
