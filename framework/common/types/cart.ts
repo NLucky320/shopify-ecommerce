8e6a791b26b36c8112ae9a4ca2c8f08c9fdad4a1
Nlucky320 <luckyes144@gmail.com>
2025-01-27 17:09:15 +0600

    normalize line item

--- framework/common/types/cart.ts
+++ framework/common/types/cart.ts
@@ -9,10 +9,10 @@ export interface Cart {
   createdAt: string
   currency: { code: string }
   taxesIncluded: boolean
-  // taxes, discounts exluded
+  // taxes, discounts excluded
   lineItemsSubtotalPrice: number
   // taxes, discounts included
   totalPrice: number
   lineItems: any[]
   discounts: Discount[]
-}
+} 
